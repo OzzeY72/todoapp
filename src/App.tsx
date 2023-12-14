28e5b8fac774c957f1b445ded3f7a9c15d12a9f1
OzzeY72 <nabacap72@gmail.com>
2023-12-14 18:16:05 +0100

    Added all handlers except Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
-import React from 'react';
+import React,{RefObject} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
+import { useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+interface TaskProps{
+  index: number;
+  order: number;
+  content: string;
+  completed: boolean;
+  readonly: boolean;
+  custRef: RefObject<HTMLInputElement>;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<TaskProps[]>(Array(0));
+
+  tasks.push({order:0,content:"Test",completed: false,custRef:React.createRef(),index:0,readonly: false})
+  tasks.push({order:1,content:"Text",completed: false,custRef:React.createRef(),index:1,readonly: true})
+
   return (
     <div className="App">
-      <Task/>
-      <Task/>
+      <Task order={tasks[0].order} content={tasks[0].content} completed={tasks[0].completed} onDelete={()=>DeleteHandler(0)} onTextInput={()=>{InputHandler(0)}} onFocusOut={()=>FocusOut(0)} onChecked={()=>{CheckedHandler(0)}} onEdit={()=>EditHandler(0)} readonly={tasks[0].readonly} index={tasks[0].index} custRef={tasks[0].custRef}/>
+      <Task order={tasks[1].order} content={tasks[1].content} completed={tasks[1].completed} onDelete={()=>DeleteHandler(1)} onTextInput={()=>{InputHandler(1)}} onFocusOut={()=>FocusOut(1)} onChecked={()=>{CheckedHandler(1)}} onEdit={()=>EditHandler(1)} readonly={tasks[1].readonly} index={tasks[1].index} custRef={tasks[1].custRef}/>
+      <button className="CreateButton" onClick={CreateHandler}>
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
     </div>
   );
+  function CreateHandler() {
+    
+  }
+  function DeleteHandler(i:number) {
+    const nextTasks = tasks.slice();
+    nextTasks.splice(i,1)
+    setTasks(nextTasks);
+  }
+  function CheckedHandler(i:number){
+    const nextTasks = tasks.slice();
+    nextTasks[i].completed = !tasks[i].completed;
+    setTasks(nextTasks);
+  }
+  function InputHandler(i:number) {
+    const nextTasks = tasks.slice();
+    nextTasks[i].content = nextTasks[i].custRef.current?.value!;
+    setTasks(nextTasks);
+  }
+  function EditHandler(i:number){
+    const nextTasks = tasks.slice();
+    nextTasks[i].readonly = false;
+    setTasks(nextTasks);
+    tasks[i].custRef.current?.focus();
+    console.log("EDit");
+  }
+  function FocusOut(i:number){
+    const nextTasks = tasks.slice();
+    nextTasks[i].readonly = true;
+    setTasks(nextTasks);
+    console.log("OUT");
+  }
 }
 
 export default App;
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler,MouseEventHandler,FocusEventHandler, RefObject } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import './Task.css';
 
-function Task() {
+export interface ITaskProps{
+  index: number;
+  order: number;
+  content: string;
+  completed: boolean;
+  custRef: RefObject<HTMLInputElement>;
+  readonly: boolean;
+  onTextInput: ChangeEventHandler<HTMLInputElement>;
+  onChecked: ChangeEventHandler<HTMLInputElement>;
+  onEdit: MouseEventHandler<HTMLButtonElement>;
+  onFocusOut: FocusEventHandler<HTMLInputElement>;
+  onDelete: MouseEventHandler<HTMLButtonElement>;
+}
+
+function Task(props:ITaskProps) {
   return (
-    <div className="Task">
-      <input className="CheckBox" type="checkbox" />
-      <div className="TextContainer">
-        <p>Lorem lo ipsum dolor sit, amet consectetur adipisicing elit. Sed impedit dolores ut dignissimos accusamus tenetur similique possimus quis maxime odit quam nemo dicta, in nisi quos soluta, saepe sapiente inventore.</p>
-      </div>
-      <button className="EditButton"><FontAwesomeIcon icon={faPencil} /></button>
-      <button className="DeleteButton"><FontAwesomeIcon icon={faTrash} /></button>
+    <div className={props.completed ? "Task completed" : "Task incompleted"} style={{order: props.order}}>
+      <input className="CheckBox" onChange={props.onChecked} type="checkbox" defaultChecked={props.completed}/>  
+      <input  ref={props.custRef} className='ContentText' value={props.content} onChange={props.onTextInput} onBlur={props.onFocusOut} readOnly={props.readonly}></input>
+      <button onClick={props.onEdit} className="EditButton"><FontAwesomeIcon icon={faPencil} /></button>
+      <button onClick={props.onDelete} className="DeleteButton"><FontAwesomeIcon icon={faTrash} /></button>
     </div>
   );
 }
