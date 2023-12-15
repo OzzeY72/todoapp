d826eac27e9e6a761fdc907d8716a9f28d6eb9c8
OzzeY72 <nabacap72@gmail.com>
2023-12-15 21:03:37 +0100

    Added constructor and create button

--- src/App.tsx
+++ src/App.tsx
@@ -13,39 +13,47 @@ interface TaskProps{
   completed: boolean;
   custRef: RefObject<HTMLInputElement>;
 }
+interface IAppState{
+  tasks: TaskProps[]
+}
 
-function App() {
-  const [tasks, setTasks] = useState<TaskProps[]>(Array(0));
-
-  tasks.push({order:0,content:"Test",completed: false,custRef:React.createRef(),index:0})
-  tasks.push({order:1,content:"Text",completed: false,custRef:React.createRef(),index:1})
-
-  return (
-    <div className="App">
-      <Task completed={tasks[0].completed} order={tasks[0].order} content={tasks[0].content} onDelete={()=>DeleteHandler(0)} onTextInput={()=>{InputHandler(0)}} onChecked={()=>{CheckedHandler(0)}} index={tasks[0].index} custRef={tasks[0].custRef}/>
-      <Task completed={tasks[1].completed} order={tasks[1].order} content={tasks[1].content} onDelete={()=>DeleteHandler(1)} onTextInput={()=>{InputHandler(1)}} onChecked={()=>{CheckedHandler(1)}} index={tasks[1].index} custRef={tasks[1].custRef}/>
-      <button className="CreateButton" onClick={CreateHandler}>
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-    </div>
-  );
-  function CreateHandler() {
-    
+class App extends React.Component<{},IAppState> {
+  constructor(props:{}){
+    super(props);
+    this.state = {
+      tasks: Array<TaskProps>(0)
+    };
   }
-  function DeleteHandler(i:number) {
-    const nextTasks = tasks.slice();
+  render() {
+    const taskElements = this.state.tasks.map((task)=><Task completed={task.completed} order={task.order} content={task.content} onDelete={()=>this.DeleteHandler(task.index)} onTextInput={()=>{this.InputHandler(task.index)}} onChecked={()=>{this.CheckedHandler(task.index)}} index={task.index} custRef={task.custRef}/>);
+    return (
+      <div className="App">
+        {taskElements}
+        <button className="CreateButton" onClick={()=>{this.CreateHandler()}}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+    );
+  }
+  CreateHandler () {
+    const nextTasks = this.state.tasks.slice();
+    nextTasks.push({order:nextTasks.length,content:"",completed: false,custRef:React.createRef(),index:nextTasks.length});
+    this.setState({tasks:nextTasks});
+  }
+  DeleteHandler(i:number) {
+    const nextTasks = this.state.tasks.slice();
     nextTasks.splice(i,1)
-    setTasks(nextTasks);
+    this.setState({tasks:nextTasks});
   }
-  function CheckedHandler(i:number){
-    const nextTasks = tasks.slice();
-    nextTasks[i].completed = !tasks[i].completed;
-    setTasks(nextTasks);
+  CheckedHandler(i:number){
+    const nextTasks = this.state.tasks.slice();
+    nextTasks[i].completed = !this.state.tasks[i].completed;
+    this.setState({tasks:nextTasks});
   }
-  function InputHandler(i:number) {
-    const nextTasks = tasks.slice();
+  InputHandler(i:number) {
+    const nextTasks = this.state.tasks.slice();
     nextTasks[i].content = nextTasks[i].custRef.current?.value!;
-    setTasks(nextTasks);
+    this.setState({tasks:nextTasks});
   }
 }
 
